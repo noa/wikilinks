261925183b200743faba772ef41ecaf958e7e770
Nicholas Andrews <noandrews@gmail.com>
2015-08-07 03:43:47 -0400

    sync

--- read.py
+++ read.py
@@ -1,31 +1,16 @@
 #! /usr/bin/env python
 
+#from urllib.parse import urlparse
+#import urllib
+#from urllib.parse import urlparse
+from urlparse import urlparse
+import os
+import argparse
 import gzip
 import zlib
-
 import pywikibot
 import pprint
 from pywikibot.data import api
-
-def getItems(site, itemtitle):
-        params = { 'action' : 'wbsearchentities',
-                   'format' : 'json',
-                   'language' : 'en',
-                   'type' : 'item',
-                   'search': itemtitle}
-        request = api.Request(site=site,**params)
-        return request.submit()
-
-def getItem(site, wdItem, token):
-        request = api.Request(site=site,
-                              action='wbgetentities',
-                              format='json',
-                              ids=wdItem)
-        return request.submit()
-
-def prettyPrint(variable):
-        pp = pprint.PrettyPrinter(indent=4)
-        pp.pprint(variable)
 
 try:
         from StringIO import StringIO
@@ -38,92 +23,78 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
-# Import thrift types
 import sys
 sys.path.append('./gen-py/edu/umass/cs/iesl/wikilink/expanded/data')
 
-#import constants
-#import ttypes
-
-#print(dir(constants))
-#print(dir(ttypes))
-
+# import thrift types
 from constants import *
 from ttypes import *
 
-# Login to Wikipedia
-wikipedia_site = pywikibot.Site("en", "wikipedia")
-page1 = pywikibot.Page(wikipedia_site, u"Douglas Adams")
-item = pywikibot.ItemPage.fromPage(page1)
+def get_args():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--input', help='compressed input', required=True)
+        parser.add_argument('--output', help='output path', required=True)
+        parser.add_argument('--lang', default='en')
+        parser.add_argument('--site', default='wikipedia')
+        return parser.parse_args()
 
-#dictionary = item.get()
-#print dictionary
+def get_site(lang, site):
+        return pywikibot.Site(lang, site)
 
-item.get()  # you need to call it to access any data.
-sitelinks = item.sitelinks
-aliases = item.aliases
-if 'en' in item.labels:
-        print('The label in English is: ' + item.labels['en'])
-if item.claims:
-        if 'P31' in item.claims: # instance of
-                print(item.claims['P31'])
-                print(len(item.claims['P31']))
-                for c in item.claims['P31']:
-                        print(str(c))
-                        print(dir(c))
-                        print(c.getTarget())
-                        print(c.target)
-                print(item.claims['P31'][0].getTarget())
-                print(item.claims['P31'][0].sources[0])  # let's just assume it has sources.
+def types_from_title(site, title):
+        try:
+                page = pywikibot.Page(site, title)
+                item = pywikibot.ItemPage.fromPage(page)
+                dictionary = item.get()
+                sitelinks = item.sitelinks
+                aliases = item.aliases
+                if item.claims:
+                        if 'P31' in item.claims: # instance of
+                                ret = []
+                                for c in item.claims['P31']:
+                                        ret.append(c.getTarget())
+                                assert(len(ret) > 0)
+                                return ret
+        except pywikibot.exceptions.NoPage:
+                return None
+        except:
+                return None
+        return None
 
-sys.exit(0)
-
-# Login to Wikidata
-wikidata_site = pywikibot.Site("wikidata", "wikidata")
-repo = wikidata_site.data_repository()
-item = pywikibot.ItemPage(repo, 'Q42')
-dictionary = item.get()
-print dictionary
-print dictionary.keys()
-#print item
-
-sys.exit(0)
-
-#token = repo.token(pywikibot.Page(repo, 'Main Page'), 'edit')
-#wikidataEntries = getItems(site, "Google")
-# Print the different Wikidata entries to the screen
-#prettyPrint(wikidataEntries)
-
-# Print each wikidata entry as an object
-#for wdEntry in wikidataEntries["search"]:
-#   prettyPrint(getItem(site, wdEntry["id"], token))
-
-decompressed_data = gzip.open('001.gz').read()
-transportIn = TTransport.TMemoryBuffer(decompressed_data)
-protocolIn = TBinaryProtocol.TBinaryProtocol(transportIn)
-item = WikiLinkItem()
-while not item == None:
-        item.read(protocolIn)
-        #print(item)
-        for m in item.mentions:
-                if not m.context == None:
-                        print('anchor text: ' + m.anchor_text)
-                        print('left:' + m.context.left)
-                        print('middle:' + m.context.middle)
-                        print('right:' + m.context.right)
-                        #print('freebase id: ' + m.freebase_id)
-                        print('wiki_url: ' + m.wiki_url)
-                        print('')
-        #serialized = transportIn.getvalue()
-        #print(type(serialized))
-
-sys.exit(0)
-
-with gzip.open('001.gz') as f:
-        file_content = f.read()
-        transportIn = TTransport.TMemoryBuffer(file_content)
+def read_mentions(inpath, outpath, site, lang):
+        ouf = open(outpath, 'w')
+        decompressed_data = gzip.open(inpath).read()
+        transportIn = TTransport.TMemoryBuffer(decompressed_data)
         protocolIn = TBinaryProtocol.TBinaryProtocol(transportIn)
         item = WikiLinkItem()
-        item.read(protocolIn)
-        serialized = transportIn.getvalue()
-        print(serialized)
+        while not item == None:
+                item.read(protocolIn)
+                for m in item.mentions:
+                        if m.context == None:
+                                continue
+                        types = None
+                        if not m.wiki_url == None:
+                                title = os.path.split(urlparse(m.wiki_url).path)[-1]
+                                types = types_from_title(site, title)
+                                if types == None:
+                                        continue
+                                t = types[0]
+                                for token in m.context.left:
+                                        ouf.write(token + ' O')
+                                type_str = t.title()
+                                print(m.anchor_text + '\t' + type_str)
+                                bio_type = "B-"+type_str
+                                for token in m.anchor_text.split():
+                                        ouf.write(m.anchor_text + ' ' + bio_type)
+                                        bio_type = "I-"+type_str
+                                for token in m.context.right:
+                                        ouf.write(token + ' O')
+                                ouf.write('\n')
+
+def main():
+        args = get_args()
+        site = get_site(args.lang, args.site)
+        read_mentions(args.input, args.output, site, args.lang)
+
+if __name__ == "__main__":
+        main()
