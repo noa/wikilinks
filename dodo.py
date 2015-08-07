8498382fdb581b2fee11619d3da376f56d0fccb3
Nicholas Andrews <noandrews@gmail.com>
2015-08-07 11:01:42 -0400

    sync

--- dodo.py
+++ dodo.py
@@ -1,12 +1,22 @@
 import os
 
+WIKI_TYPES='wiki_types'
+
+def task_wiki_types():
+    return {
+        'actions': ''
+    }
+
 def task_thrift2conll():
     for file in sorted(os.listdir(".")):
         if file.endswith(".gz"):
             name = os.path.basename(file)
             base_name = os.path.splitext(name)[0]
+            out_file = base_name+".conll"
             yield {
                 'basename': 'thrift2conll',
                 'name': base_name,
-                'actions': ['python read.py --input %s --output %s' % (file, base_name+".conll")]
+                'actions': ['python read.py --input %s --output %s --types %s'
+                            % (file, out_file, wiki_types)],
+                'targets': [out_file]
             }
